167bc36d8ba8b061ac4ccd19e75353e46250bd69
SEGUYMIMI <78414319+SEGUYMIMI@users.noreply.github.com>
2021-10-04 22:16:49 +0000

    Add set_active_route helper

--- app/helpers.php
+++ app/helpers.php
@@ -17,3 +17,16 @@
  }
 
 
+ if(! function_exists('set_active_route')) 
+ {
+    function set_active_route($route)
+        
+    {
+        return Route::is($route) ? 'active' : ''; 
+        
+        
+        
+        
+    }
+ }
+
