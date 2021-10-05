f625fcbbbe68cf0a636371e58d2d474a8aaf8dc3
SEGUYMIMI <78414319+SEGUYMIMI@users.noreply.github.com>
2021-10-05 11:50:11 +0000

    Configure app name

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="text-center">
-   <p> &copy; {{ date('Y') }} &middot;  Laracarte by <a href="https://twitter.com/etsmo"></a> @etsmo 
+   <p> &copy; {{ date('Y') }} &middot; {{ config('app.name') }} by <a href="https://twitter.com/etsmo"></a> @etsmo 
    &dash; A <a href="https://laramap.com"> laramap</a> clone app.</P>
    <p><b>this app has been built for learning purpose.</b></p> 
 
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="container">
-   <h2> What is Laracarte?</h2>
+   <h2> What is {{ config('app.name') }}?</h2>
    <p>Laracarte is a clone of app <a href="https://laramap.com" target="_blank">laramap.com</a>.</p> 
 
    <div class="row">
@@ -20,13 +20,13 @@
     <hr>
 
     <h2>What is Laramap?</h2>
-    <p>Laramap is the website by which Laracarte was inspired:) .</p>
+    <p>Laramap is the website by which {{ config('app.name') }} was inspired:) .</p>
     <p>More info  <a href="https://laramap.com/p/about"></a>here.</p>
 
     <hr>
 
 
-    <h2> Which tools and services are used Laracarte?</h2>
+    <h2> Which tools and services are used {{ config('app.name') }}?</h2>
 
     <p> Basically it's built on Laravel &amp; Bootstrap. But there's a bunch of services used for
      email and other sections.  </p>
